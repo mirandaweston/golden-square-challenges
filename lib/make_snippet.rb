b121c910522e697c382417889e06cc249f331f8b
mirandaweston <mweston105@yahoo.co.uk>
2023-01-18 15:11:01 +0000

    phase 2: TDD make_snippet method second test

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,3 +1,10 @@
 def make_snippet(string)
-    return string
+    words = string.split(" ")
+      if words.length <= 5
+        return words.join(" ")
+      else
+        under_five = words.slice(0, 5)
+        words = under_five.join(" ") + "..."
+      end
+    return words
 end
