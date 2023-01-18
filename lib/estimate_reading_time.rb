5663d9dd5116e6468cd87fb87d31caa59d1ebdb8
mirandaweston <mweston105@yahoo.co.uk>
2023-01-18 18:20:29 +0000

    single method design recipe phase 2: estimate_reading_time test 2

--- lib/estimate_reading_time.rb
+++ lib/estimate_reading_time.rb
@@ -1,6 +1,8 @@
 def estimate_reading_time(text)
   words = text.split(" ").length
     if words == 0
+      return 0
+    else
+      return 1
     end
-  return 0
 end
--- spec/estimate_reading_time_spec.rb
+++ spec/estimate_reading_time_spec.rb
@@ -1,8 +1,13 @@
 require 'estimate_reading_time'
 
 RSpec.describe "user enters text and method returns estimate reading time" do
-    it "returns estimate reading time of given text" do
+    it "returns 0 when given text is empty" do
         reading_time = estimate_reading_time("")
         expect(reading_time).to eq 0
+    end
+
+    it "returns 1 when given text is 'one'" do
+        reading_time = estimate_reading_time("one")
+        expect(reading_time).to eq 1
     end
 end
