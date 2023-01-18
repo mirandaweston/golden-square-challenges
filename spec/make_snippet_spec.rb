4c1d4ecca65841ac840e2c0014691c63422d047d
mirandaweston <mweston105@yahoo.co.uk>
2023-01-18 15:05:44 +0000

    phase 2: TDD make_snippet method second test

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -6,3 +6,10 @@ RSpec.describe "takes a string as an argument and returns the first five words" 
         expect(result).to eq "Hello world, how are you?"
     end
 end
+
+RSpec.describe "takes a string as an argument and returns string + '...'" do
+    it "returns the first five words plus '...'" do
+        result = make_snippet("Good morning world, how are you?")
+        expect(result).to eq "Good morning world, how are..."
+    end
+end
