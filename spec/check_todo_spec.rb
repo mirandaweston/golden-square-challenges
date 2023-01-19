93a201883275ce2efec917a103b89b5738400df2
mirandaweston <mweston105@yahoo.co.uk>
2023-01-19 19:53:02 +0000

    check_todo method and full test suite

--- spec/check_todo_spec.rb
+++ spec/check_todo_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe "check_todo method" do
         expect(check_todo("")).to eq false
     end
 
-    it "returns true if the text give includes '#TODO'" do
+    it "returns true if the text given includes '#TODO'" do
         expect(check_todo("#TODO write code")).to eq true
+    end
+
+    it "returns true if the text given includes '#TODO'" do
+        expect(check_todo("food shop #TODO")).to eq true
+    end
+
+    it "returns true if the text given includes '#TODO'" do
+        expect(check_todo("buy dog food #TODO feed dog")).to eq true
+    end
+
+    it "returns false if the text given does not include '#TODO'" do
+        expect(check_todo("it is raining")).to eq false
     end
 end
