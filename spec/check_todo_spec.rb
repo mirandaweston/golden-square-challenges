0e885af9c681ffc6d0b884fb911fd59e344c6ebf
mirandaweston <mweston105@yahoo.co.uk>
2023-01-19 19:45:46 +0000

    check_todo method and first test

--- spec/check_todo_spec.rb
+++ spec/check_todo_spec.rb
@@ -1,7 +1,7 @@
 require 'check_todo'
 
 RSpec.describe "check_todo method" do
-    it "returns false if the text given is an empty string" do
+    it "returns false if the text given is an empty array" do
         expect(check_todo("")).to eq false
     end
 end
