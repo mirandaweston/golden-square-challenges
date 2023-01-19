eaf67629042fac632f0b5823b7121cdcfd61925f
mirandaweston <mweston105@yahoo.co.uk>
2023-01-19 11:17:41 +0000

    single method design recipe: grammar_check second test

--- lib/grammar_check.rb
+++ lib/grammar_check.rb
@@ -1,5 +1,5 @@
 def grammar_check(text)
-    if text[0].capitalize && text[-1] == "."
+    if text[0].capitalize && text[-1] == "." || text[-1] == "?"
         return true
     end
 end
--- spec/grammar_check_spec.rb
+++ spec/grammar_check_spec.rb
@@ -1,8 +1,13 @@
 require 'grammar_check'
 
 RSpec.describe "user enters text and method returns boolean to show whether text is grammatically correct" do
-  it "returns true if text ends with a '.' and is capitalized" do
+  it "returns true if text ends with an '.' and is capitalized" do
     result = grammar_check("It is sunny today.")
+    expect(result). to eq true
+  end
+
+  it "returns true if text ends with an '?' and is capitalized" do
+    result = grammar_check("It's sunny today?")
     expect(result). to eq true
   end
 end
