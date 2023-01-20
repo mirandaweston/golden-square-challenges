74584a149485b397ee392c1dfd3a681b91127bf0
mirandaweston <mweston105@yahoo.co.uk>
2023-01-20 00:18:41 +0000

    exercise 2: debugged first method

--- Debugging-1/Exercise_2.rb
+++ Debugging-1/Exercise_2.rb
@@ -1,15 +1,7 @@
 def encode(plaintext, key)
     cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
-    p key.chars.uniq
-    p ('a'..'z').to_a
-    p ('a'..'z').to_a - key.chars
-    exit
     ciphertext_chars = plaintext.chars.map do |char|
-        p "cipher.find_index(char):"
-        p cipher.find_index(char)
-        p char
-        p cipher
-      (65 + cipher.find_index(char)).chr
+        (65 + cipher.find_index(char)).chr
     end
     return ciphertext_chars.join
   end
