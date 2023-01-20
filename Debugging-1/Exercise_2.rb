1338564b2009787d69cc88884631768ada1aafe6
mirandaweston <mweston105@yahoo.co.uk>
2023-01-20 00:31:04 +0000

    exercise 2: debugging method 2

--- Debugging-1/Exercise_2.rb
+++ Debugging-1/Exercise_2.rb
@@ -7,14 +7,21 @@ def encode(plaintext, key)
   end
   
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     plaintext_chars = ciphertext.chars.map do |char|
-      cipher[65 - char.ord]
+      p char
+      p char.ord
+      p cipher
+      out_char = cipher[char.ord - 65]
+      p "should be 'h'"
+      p "is #{out_char}"
+      exit
     end
     return plaintext_chars.join
   end
 
   puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+  puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
   
   # Intended output:
   #
