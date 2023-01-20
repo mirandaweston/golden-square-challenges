67e03230d7ac16299819eedc21fb7e0baf89341f
mirandaweston <mweston105@yahoo.co.uk>
2023-01-20 16:24:05 +0000

    second test: count_words method

--- test-drive-classes/spec/DiaryEntry_spec.rb
+++ test-drive-classes/spec/DiaryEntry_spec.rb
@@ -2,8 +2,13 @@ require 'DiaryEntry'
 
 RSpec.describe DiaryEntry do
     it "constructs" do
-        diary_entry = DiaryEntry.new("title", "contents")
-        expect(diary_entry.title).to eq "title"
-        expect(diary_entry.contents).to eq "contents"
+        @diary_entry = DiaryEntry.new("title", "contents")
+        expect(@diary_entry.title).to eq "title"
+        expect(@diary_entry.contents).to eq "contents"
+    end
+
+    it "returns the number of words in the contents as an integer" do
+        @diary_entry = DiaryEntry.new("title", "contents")
+        expect(@diary_entry.count_words).to eq 1
     end
 end
