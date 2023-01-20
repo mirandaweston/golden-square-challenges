d86c4ccc50988ecd194b3a740e7c6eb1137a8381
mirandaweston <mweston105@yahoo.co.uk>
2023-01-20 12:26:32 +0000

    Challenge: get_most_common_letter debugged

--- Debugging-1/challenge.rb
+++ Debugging-1/challenge.rb
@@ -4,7 +4,7 @@ def get_most_common_letter(text)
     text.chars.each do |char|
         counter[char] += 1
     end
-    p counter.to_a.sort_by { |k, v| v }[-1][0]
-end
+    counter.to_a.sort_by { |k, v| v }[-1][0]
+ end
 
-puts get_most_common_letter("the roof, the roof, the roof is on fire!")
+ puts get_most_common_letter("the roof, the roof, the roof is on fire!")
