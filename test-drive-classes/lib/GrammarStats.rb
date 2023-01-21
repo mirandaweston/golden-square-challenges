188fe4fe7084edeff7de7c383b4ee0111170dbc9
mirandaweston <mweston105@yahoo.co.uk>
2023-01-21 16:00:22 +0000

    class GrammarStats method refactors

--- test-drive-classes/lib/GrammarStats.rb
+++ test-drive-classes/lib/GrammarStats.rb
@@ -1,12 +1,26 @@
 class GrammarStats
     def initialize
+        @texts = []
     end
 
     def check(text)
         if text[0] == text[0].upcase && (text[-1] == "." || text[-1] == "?" || text[-1] == "!")
-          return true
+            @texts.push("true")
+            return true
         else
+            @texts.push("false")
             return false
         end
+    end
+
+    def percentage_good
+        count = 0
+
+        @texts.each do |word|
+            if word == "true"
+                count += 1
+            end
+        end
+        return percentage = (count * 100) / @texts.length
     end
 end
