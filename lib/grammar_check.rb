adcb9e2555c4aca5e9ff3ea0b802db3a116bb80d
mirandaweston <mweston105@yahoo.co.uk>
2023-01-21 13:26:00 +0000

    grammar_check method refactoring

--- lib/grammar_check.rb
+++ lib/grammar_check.rb
@@ -1,9 +1,8 @@
 def grammar_check(text)
+  def check(text)
     if text[0] == text[0].upcase && (text[-1] == "." || text[-1] == "?" || text[-1] == "!")
       return true
-    elsif text[0] == text[0].downcase && text[-1] == "."
+    else
       return false
-    elsif text[0] == text[0].upcase && (text[-1] != "." || "!" || "?")
-      return false
-    end
+  end
 end
