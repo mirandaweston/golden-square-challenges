5cef8f6a2580323328de16bedafb527532c03bcf
mirandaweston <mweston105@yahoo.co.uk>
2023-01-22 16:43:52 +0000

    LetterCounter code debugged

--- debugging-2/LetterCounter_challenge.rb
+++ debugging-2/LetterCounter_challenge.rb
@@ -4,22 +4,25 @@ class LetterCounter
     end
   
     def calculate_most_common()
-      counter = Hash.new(1)
+      counter = Hash.new(0)
+      #p counter
       most_common = nil
       most_common_count = 1
       @text.chars.each do |char|
         next unless is_letter?(char)
         counter[char] = (counter[char] || 1) + 1
+        #binding.irb
         if counter[char] > most_common_count
           most_common = char
-          most_common_count += counter[char]
+          most_common_count = counter[char]
+        #p char
         end
       end
       return [most_common_count, most_common]
     end
   
     private
-  
+
     def is_letter?(letter)
       return letter =~ /[a-z]/i
     end
