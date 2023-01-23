47b9b77e7b06faf1808c9d0cd7f4688712742c09
mirandaweston <mweston105@yahoo.co.uk>
2023-01-23 22:49:51 +0000

    added tests for count_words and diary_entry_reading_time

--- test-multi-class/spec/diary_entry_spec.rb
+++ test-multi-class/spec/diary_entry_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe DiaryEntry do
         diary_entry = DiaryEntry.new("day_1", "some contents")
         expect(diary_entry.contents).to eq "some contents"
     end
+
+    it "returns the number of words in the contents as an integer" do
+        diary_entry = DiaryEntry.new("day_1", "some contents")
+        expect(diary_entry.count_words).to eq 2
+    end
+
+    it "returns the reading time for the contents in minutes" do
+        diary_entry = DiaryEntry.new("day_1", "some contents")
+        expect(diary_entry.reading_time(1)).to eq 2
+    end
 end
