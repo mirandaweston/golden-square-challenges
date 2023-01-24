993916512beb4f6187b80aa7eacb79696d67a49a
mirandaweston <mweston105@yahoo.co.uk>
2023-01-24 18:22:53 +0000

    adding comments to Diary class

--- test-multi-class/lib/diary.rb
+++ test-multi-class/lib/diary.rb
@@ -11,19 +11,19 @@ class Diary
         return @entries
     end
 
-    def count_words
+    def count_words #returns the number of words in all diary entries
         @entries.collect(&:count_words).sum
     end
 
-    def reading_time(wpm)
-        (count_words / wpm.to_f).ceil
+    def reading_time(wpm) #wpm is an integer representing the number of words the user can read per min
+        (count_words / wpm.to_f).ceil #returns an integer representing an estimate reading time to read all entries
     end
 
-    def find_best_entry_for_reading_time(wpm, minutes)
+    def find_best_entry_for_reading_time(wpm, minutes) #minutes is an integer representing number of minutes the user has to read
         max_length = wpm * minutes
         readable = @entries.select do |entry|
             entry.count_words <= max_length
         end
-        readable.max_by(&:count_words)
+        readable.max_by(&:count_words) #returns entry closest to (but not over) the length the user could read in time available
     end
 end
