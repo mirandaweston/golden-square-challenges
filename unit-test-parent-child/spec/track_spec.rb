71c36aca23ee6b2b21d83879de367ca56f257332
mirandaweston <mweston105@yahoo.co.uk>
2023-01-25 17:12:09 +0000

    adding test suite for Track class

---
+++ unit-test-parent-child/spec/track_spec.rb
@@ -0,0 +1,23 @@
+require 'track'
+
+RSpec.describe Track do
+    it "constructs" do
+        track_1 = Track.new("title_1", "artist_1")
+        track_2 = Track.new("title_2", "artist_2")
+    end
+
+    it "returns true if given a keyword in the title" do
+        track = Track.new("title_1", "artist_1")
+        expect(track.matches?("title_1")).to eq true
+    end
+
+    it "returns true if given a keyword in the artist" do
+        track = Track.new("title_2", "artist_2")
+        expect(track.matches?("artist_2")).to eq true
+    end
+
+    it "returns false when given a keyword which is not present in title or artist" do
+        track = Track.new("title_3", "artist_3")
+        expect(track.matches?("title")).to eq false
+    end
+end
