75b3c03f4984b6e9ffbb7bc4a01c2300036acaa7
mirandaweston <mweston105@yahoo.co.uk>
2023-01-25 19:27:41 +0000

    debugging MusicLibrary integration test suite

--- unit-test-parent-child/spec/musiclibrary_integration_spec.rb
+++ unit-test-parent-child/spec/musiclibrary_integration_spec.rb
@@ -4,14 +4,14 @@ require 'track'
 RSpec.describe "Music Library Integration" do
     it "constructs" do
         music_library = MusicLibrary.new
-        track_1 = Track.new("title", "artist")
-        track_2 = Track.new("title", "artist")
+        track_1 = Track.new("title_1", "artist_1")
+        track_2 = Track.new("title_2", "artist_2")
     end
 
     it "adds a track to the library and returns a list of tracks" do
         music_library = MusicLibrary.new
-        track_1 = Track.new("title", "artist")
-        track_2 = Track.new("title_1", "artist_1")
+        track_1 = Track.new("title_1", "artist_1")
+        track_2 = Track.new("title_2", "artist_2")
         music_library.add(track_1)
         music_library.add(track_2)
         expect(music_library.all).to eq [track_1, track_2]
@@ -19,12 +19,12 @@ RSpec.describe "Music Library Integration" do
 
     it "passes a keyword as a string and searches for it within the library" do
         music_library = MusicLibrary.new
-        track_1 = Track.new("title", "artist")
-        track_2 = Track.new("title_1", "artist_1")
+        track_1 = Track.new("title_1", "artist_1")
+        track_2 = Track.new("title_2", "artist_2")
         music_library.add(track_1)
         music_library.add(track_2)
         music_library.all
-        track_1.matches?(track_1)
-        expect(music_library.search(track_1)).to eq [track_2]
+        track_1.matches?("title_1")
+        expect(music_library.search("title_1")).to eq [track_1]
     end
 end
