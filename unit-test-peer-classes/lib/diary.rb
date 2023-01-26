ef35109a1ec09ac29dccca7a2d4a4b2451d7cfa9
mirandaweston <mweston105@yahoo.co.uk>
2023-01-26 13:00:22 +0000

    adding Diary class

---
+++ unit-test-peer-classes/lib/diary.rb
@@ -0,0 +1,10 @@
+class Diary
+    def initialize(contents) # contents is a string
+      @contents = contents
+    end
+  
+    def read
+      # Returns the contents of the diary
+      return @contents
+    end
+  end
