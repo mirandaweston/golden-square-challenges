1817700a6ee8d5585343e8c6d4054bb611477dc9
mirandaweston <mweston105@yahoo.co.uk>
2023-01-26 13:43:52 +0000

    adding next test for TaskFormatter class

--- unit-test-parent-child/spec/task_formatter_spec.rb
+++ unit-test-parent-child/spec/task_formatter_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe TaskFormatter do
         formatter = TaskFormatter.new(fake_task)
         expect(formatter.format).to eq "- [ ] walk the dog"
     end
+
+    it "formats the task as a string when task is complete" do
+        fake_task = double(:fake_task, title: "walk the dog", complete?:true)
+        formatter = TaskFormatter.new(fake_task)
+        expect(formatter.format).to eq "- [x] walk the dog"
+    end
 end
