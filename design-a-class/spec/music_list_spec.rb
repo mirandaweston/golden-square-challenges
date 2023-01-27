ff69590b4673b4313ce1367bd6eb15e31415393a
mirandaweston <mweston105@yahoo.co.uk>
2023-01-27 02:11:09 +0000

    adding test suite for MusicList class

---
+++ design-a-class/spec/music_list_spec.rb
@@ -0,0 +1,20 @@
+require 'music_list'
+
+RSpec.describe MusicList do
+  it "adds a track to the music list" do
+    music_list = MusicList.new
+    expect(music_list.add("track_1")).to eq ["track_1"]
+  end
+
+  it "returns an empty list when no track is added" do
+    music_list = MusicList.new
+    expect(music_list.track_list).to eq []
+  end
+
+  it "returns a list of all the tracks added to the music list" do
+    music_list = MusicList.new
+    music_list.add("track_1")
+    music_list.add("track_2")
+    expect(music_list.track_list).to eq ["track_1", "track_2"]
+  end
+end
