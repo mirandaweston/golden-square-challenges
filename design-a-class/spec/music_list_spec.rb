10566516b651530f7986bcd8079acd10c4be9a87
mirandaweston <mweston105@yahoo.co.uk>
2023-01-27 02:28:33 +0000

    MusicList test suite: second test

--- design-a-class/spec/music_list_spec.rb
+++ design-a-class/spec/music_list_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe MusicList do
     music_list = MusicList.new
     expect(music_list.add("track_1")).to eq ["track_1"]
   end
+
+  it "returns an empty list when no track is added to the music list" do
+    music_list = MusicList.new
+    expect(music_list.list).to eq []
+  end
 end
